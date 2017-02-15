bdf7a2b02ee5e8970780a1bd1a691d8001f994c5
francisco.diaz <francisco.diaz@unodc.org>
2017-02-15 17:27:01 +0100

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,56 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http) {
+  $http.get('/users').then(
+    function(users) {
+      console.log(users);
+      
+      var client = Stomp.client('ws://localhost:61614');
+
+      client.connect(
+        {
+          'client-id': 'test-client-id'
+        },
+        function() {
+          client.subscribe(
+            '/topic/T.ACCESSCONTROL.PERSON.DEF.OUT',
+            function(message) {
+              var obj = JSON.parse(message.body);
+              console.log(obj);
+            },
+            {
+              'activemq.subscriptionName': 'test-subscription'
+            }
+          );
+        },
+        function(err) {
+          console.error(err);
+        }
+      );
+  });
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
